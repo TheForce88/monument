5f19179dc619cca32ed03c8e42361046345a9cde
eches <Elaine.Chesoni83@gmail.com>
2018-04-16 19:20:05 -0700

    updated styling

--- js/main.js
+++ js/main.js
@@ -1,13 +1,40 @@
-// var inputSelector = $('input');
-//
-// inputSelector.on('keyup', function(){
-//   inputSelector.addClass('error');
-// });
+$(document).ready(function() {
+  $('.contact').on('submit', function (evt) {
+    evt.preventDefault();
 
-$('.submit').on('click', function(){
-  var inputSelector = $('input');
+    // Code for validating the Forms
+    $('form input, form textarea').each(function() {
+      var $currentField = $(this);
+      var fieldType = $(this).attr('type');
 
-  inputSelector.on('keyup', function(){
-    inputSelector.addClass('error');
+      if ($currentField.val() === '' ){
+        $currentField.addClass('error');
+        $currentField.siblings('.error-message').fadeIn();
+        return;
+      }
+
+      if (fieldType === 'email') {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        var isValidEmailAddress = re.test($currentField.val());
+        if (!isValidEmailAddress) {
+          $currentField.addClass('error');
+          $currentField.siblings('.error-message').text('Please enter a valid email address.').fadeIn();
+          return;
+        }
+      }
+      $currentField.removeClass('error');
+      $currentField.siblings('.error-message').hide();
+    });
   });
+
+  $('nav a').on('click', function(e) {
+    // console.log('you clicked on a nav bar item');
+    e.preventDefault();
+    var thisTarget = $(this).attr('href');
+    var targetOffset = $(thisTarget).offset().top;
+    $('html, body').animate({
+      scrollTop: targetOffset
+    }, 600);
+  });
+
 });
